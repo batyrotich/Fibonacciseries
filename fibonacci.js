8d6c4bcc5ad93edcd696cf5d60b7312ac2d8f38d
Chebet Eddah <66619709+batyrotich@users.noreply.github.com>
2022-02-14 00:16:47 +0300

    created a function in the fibonacci.js file

---
+++ fibonacci.js
@@ -0,0 +1,17 @@
+function fibonacci(x){
+    let fn1=0;
+    let fn=1;
+  
+    while(fn <= x){
+  
+      if (fn == x){
+        return x + " is a fibonacci Series";
+      }
+     fn += fn1;
+      fn1= fn-fn1;
+      }
+     return x + " Does not belong to fibonacci series";
+    }
+  
+  var x = prompt("Input any number");
+  document.write(fibonacci(x));
